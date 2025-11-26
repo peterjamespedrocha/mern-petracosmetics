2010d5926eea468746dc68e4ee790a4e0bf20799
peterjamespedrocha <peci.pedrocha.up@phinmaed.com>
2025-11-26 16:36:58 +0800

    Update API URLs to production environment

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -1,6 +1,8 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
+const API_URL = "https://petracosmetics-backend.onrender.com";
+
 const initialState = {
   isAuthenticated: false,
   isLoading: true,
@@ -12,7 +14,7 @@ export const registerUser = createAsyncThunk(
 
   async (formData) => {
     const response = await axios.post(
-      "http://localhost:5000/api/auth/register",
+      "https://petracosmetics-backend.onrender.com/api/auth/register",
       formData,
       {
         withCredentials: true,
@@ -28,7 +30,7 @@ export const loginUser = createAsyncThunk(
 
   async (formData) => {
     const response = await axios.post(
-      "http://localhost:5000/api/auth/login",
+      "https://petracosmetics-backend.onrender.com/api/auth/login",
       formData,
       {
         withCredentials: true,
@@ -44,7 +46,7 @@ export const logoutUser = createAsyncThunk(
 
   async () => {
     const response = await axios.post(
-      "http://localhost:5000/api/auth/logout",
+      "https://petracosmetics-backend.onrender.com/api/auth/logout",
       {},
       {
         withCredentials: true,
@@ -60,7 +62,7 @@ export const checkAuth = createAsyncThunk(
 
   async () => {
     const response = await axios.get(
-      "http://localhost:5000/api/auth/check-auth",
+      "https://petracosmetics-backend.onrender.com/api/auth/check-auth",
       {
         withCredentials: true,
         headers: {
