18c9a5c97c551f07fc2b85662cc89eac7a248341
peterjamespedrocha <peci.pedrocha.up@phinmaed.com>
2025-11-26 17:21:42 +0800

    Remove hardcoded API_URL from products-slice
    Removed hardcoded API_URL definition.

--- client/src/store/shop/products-slice/index.js
+++ client/src/store/shop/products-slice/index.js
@@ -1,8 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
-
 const initialState = {
   isLoading: false,
   productList: [],
