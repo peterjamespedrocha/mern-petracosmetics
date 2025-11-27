0c269065328824944fd3890cfb0d7601158ed690
peterjamespedrocha <peci.pedrocha.up@phinmaed.com>
2025-11-27 15:45:44 +0800

    Update API URL for search results fetching

--- client/src/store/shop/search-slice/index.js
+++ client/src/store/shop/search-slice/index.js
@@ -1,6 +1,8 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+
 const initialState = {
   isLoading: false,
   searchResults: [],
@@ -10,7 +12,7 @@ export const getSearchResults = createAsyncThunk(
   "/order/getSearchResults",
   async (keyword) => {
     const response = await axios.get(
-      `http://localhost:5000/api/shop/search/${keyword}`
+      `${API_URL}/api/shop/search/${keyword}`
     );
 
     return response.data;
