3ceb9941c4e19a7612d7ab108721bc133e41e0e5
peterjamespedrocha <peci.pedrocha.up@phinmaed.com>
2025-11-27 15:46:46 +0800

    Update API URL to use environment variable

--- client/src/store/common-slice/index.js
+++ client/src/store/common-slice/index.js
@@ -1,6 +1,8 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+
 const initialState = {
   isLoading: false,
   featureImageList: [],
@@ -10,7 +12,7 @@ export const getFeatureImages = createAsyncThunk(
   "/order/getFeatureImages",
   async () => {
     const response = await axios.get(
-      `http://localhost:5000/api/common/feature/get`
+      `${API_URL}/api/common/feature/get`
     );
 
     return response.data;
@@ -21,7 +23,7 @@ export const addFeatureImage = createAsyncThunk(
   "/order/addFeatureImage",
   async (image) => {
     const response = await axios.post(
-      `http://localhost:5000/api/common/feature/add`,
+      `${API_URL}/api/common/feature/add`,
       { image }
     );
 
