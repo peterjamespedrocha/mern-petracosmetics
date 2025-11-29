e967ef04c0be171f1fc6cfda0f22f6f532aed035
peterjamespedrocha <peci.pedrocha.up@phinmaed.com>
2025-11-29 15:38:06 +0800

    Fix cookie response formatting in auth-controller

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -71,7 +71,7 @@ res.cookie("token", token, {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
   sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
-});
+})
   .json({
     success: true,
     message: "Logged in successfully",
